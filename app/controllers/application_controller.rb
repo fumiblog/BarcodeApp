333d060bb6f6c466c667f5b7dbd31d74daa02057
fumiblog <fumihisa.yano@gmail.com>
2020-12-22 06:22:43 +0000

    [login、memberid表示] 2-2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+    def after_sign_in_path_for(resource)
+        case resource
+        when User
+            barcodes_show_path
+        when Admin
+            barcodes_index_path
+        end
+    end
+
+    def after_sign_out_path_for(resource)
+        # byebug
+        case resource
+        when :user
+            barcodes_index_path
+        when :admin
+            barcodes_index_path
+        end
+    end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+
 end
--- app/controllers/barcodes_controller.rb
+++ app/controllers/barcodes_controller.rb
@@ -1,6 +1,13 @@
 class BarcodesController < ApplicationController
-  
+
   def index
      @barcode = Barcode.new(490333304734)
   end
+
+  def show
+    @number = 290000000000 + current_user.id
+    @barcode = Barcode.new(@number)
+  end
+
+
 end
